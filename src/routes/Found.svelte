30b6773f448b1c47fcd30909636923a65fb09c73
aarsh21 <aarshpersonal21@gmail.com>
2023-07-26 21:55:06 +0530

    add : gets a transition when user finds a pait.

--- src/routes/Found.svelte
+++ src/routes/Found.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
+	import { receive } from './transitions';
 	import { getTwemojiUrl } from './utils';
 
 	export let found: string[];
 </script>
 
 <div class="found">
-	#{#each found as emoji (emoji)}
-		<img src={getTwemojiUrl(emoji)} alt={emoji} />
+	{#each found as emoji (emoji)}
+	<div class="pair">
+		<img 
+		in:receive={{key:`${emoji}:a`}}	
+		src={getTwemojiUrl(emoji)} 
+		alt={emoji} />
+		<img 
+		in:receive={{key:`${emoji}:b`}}	
+		src={getTwemojiUrl(emoji)} 
+		alt={emoji} />
+	</div>
 	{/each}
 </div>
 
@@ -16,10 +26,25 @@
 		justify-content: center;
 		align-items: center;
 		gap: 0.5em;
+		height: 100%;
+		filter: drop-shadow( rgba(9, 30, 66, 0.25) 3px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px);
 	}
 
-	img {
-		width: 4em;
-		height: 4em;
+	.pair{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 8em;
+		height: 8em;
+		background: rgb(223, 222, 222);
+		padding: 1em;
+		border-radius: 50%; 
+		filter: drop-shadow( rgba(9, 30, 66, 0.25) 3px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px);
+	}
+
+	img {  
+		position: absolute;
+		width:4em;
+		height:4em;
 	}
 </style>
--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -36,6 +36,9 @@
 			}}
 			selected={a === i || b === i}
 			found={found.includes(emoji)}
+			group={
+				grid.indexOf(emoji) === i? 'a' : 'b'
+			}
 		/>
 	{/each}
 </div>
